cc8328615cb9a309d6460621c719bde521c7e67c
Sawjal <mdsajalsikder@gmail.com>
2023-11-07 00:37:15 +0600

    use React Router navbar

--- src/Componant/Route/Home.jsx
+++ src/Componant/Route/Home.jsx
@@ -3,21 +3,25 @@ import React from 'react';
 import Home from "./Pages/Home";
 import Blog from "./Pages/Blog";
 import Contract from "./Pages/Contract";
+import Error from "./Pages/Error";
 
 // frist install React router dom this ( npm install react-router-dom )
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './Navbar';
 
 const RouterApp = () => {
   return (
-    <div>
+   
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route  path='/' element={ <Home /> } />
                 <Route  path='/blog' element={ <Blog /> } />
                 <Route  path='/contract' element={ <Contract /> } />
+                <Route  path='*' element={ <Error /> } />
             </Routes>
         </BrowserRouter>
-    </div>
+
   );
 }
 
--- src/Componant/Route/Pages/Contract.jsx
+++ src/Componant/Route/Pages/Contract.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Contract = () => {
+  const navgate = useNavigate()
   return (
     <div>
       <h1>Contracts</h1>
+      <button onClick={()=>{navgate("/")}} >Go To Home Page</button>
     </div>
   );
 }
